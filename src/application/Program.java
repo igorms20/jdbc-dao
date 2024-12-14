1b64d60a7b3119d2b88df1cdb75c1414fd0ab34e
André Igor <andreigormenezes@gmail.com>
2024-12-14 15:33:06 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.time.LocalDateTime;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		
+		Department department = new Department(1, "Books");
+		Seller seller = new Seller(1, "Roger", "roger@gmail.com", LocalDateTime.now(), 3500.00, department);
 		
 
 	}
